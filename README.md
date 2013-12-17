26bdc88751fddadcfb7c745ff6ec8a1213c200cb
Jens Nilsson <jens.nilsson@24hr.se>
2013-12-17 11:17:32 +0100

    Added support for content-part of the API, bumped version to 1.1.0

--- README.md
+++ README.md
@@ -8,16 +8,19 @@ Install with npm:
 `npm install visitsweden-places-api`
 
 ##Usage
-Simple usage example:
+Simple usage examples:
 
+###Creating the client
 	var placesClient = require("visitsweden-places-api");
 	
 	// you need to specify your appKey when creating the client
 	var client = new placesClient({
 		appKey: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
 	});
+
+###Places
 	
-	// search for places of type "region" and with a name that matches "land"
+	// search for places of type "region" and with a name contains "land"
 	var params = {
 		types: "region",
 		name: "land"
@@ -34,4 +37,20 @@ Simple usage example:
 	client.getPlace(id, params, function(err, json){
 		
 	});
+###Content
+	// search for content that has a name containing "winter"
+	var params = {
+		name: "winter"
+	};
 
+	client.searchContent(params, function(err, json){
+
+	});
+
+	// get content with id XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+	var params = {};
+	var id = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
+
+	client.getContent(id, params, function(err, json){
+
+	});
--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var querystring = require('qs');
 
 var placesClient = module.exports = function( config ) {
     this.config = config;
-    this.searchUrl = 'http://api.places.visitsweden.com/places/';
-    this.placeUrl = this.searchUrl + 'place/';
-    this.autocompleteUrl = this.searchUrl + 'autocomplete/';
+    this.baseUrl = 'http://api.places.visitsweden.com/'; // base url for the API
+    this.searchUrl = this.baseUrl + 'places/'; // search for places
+    this.placeUrl = this.searchUrl + 'place/'; // single place
+    this.autocompleteUrl = this.searchUrl + 'autocomplete/'; // autocomplete places
+    this.searchContentUrl = this.baseUrl + 'contents/'; // search content
+    this.contentUrl = this.searchContentUrl + 'content/'; // single content
 
     // http://places.visitsweden.com/Reference/PlacesAPI.aspx#searchingplaces
     this.searchPlaces = function(params, callback) {
@@ -29,6 +32,24 @@ var placesClient = module.exports = function( config ) {
     this.autoComplete = function(params, callback) {
 
         var url = this.autocompleteUrl;
+
+        this.makeRequest(url, params, callback);
+
+    };
+
+    // http://places.visitsweden.com/Reference/ContentAPI.aspx#searchingcontents
+    this.searchContent = function(params, callback) {
+
+        var url = this.searchContentUrl;
+
+        this.makeRequest(url, params, callback);
+
+    };
+
+    // http://places.visitsweden.com/Reference/ContentAPI.aspx#singlecontent
+    this.getContent = function(id, params, callback) {
+
+        var url = this.contentUrl + id;
 
         this.makeRequest(url, params, callback);
 
